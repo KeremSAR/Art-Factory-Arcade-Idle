d642ffc73f59b897f822639e9ef364fc269d8abe
KeremSAR <98567140+KeremSAR@users.noreply.github.com>
2022-09-17 14:09:25 +0300

    adding Systems prefab for inital of the project

---
+++ ArtFactory3D/Assets/Scripts/Managers/Systems/SystemsOnLoad.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace ArtFactory.Managers.Systems
+{
+    /* Loads the system prefab before on loading scene, it can be generated a subobjects inside System prefab and it will instantiates in every scene*/
+    public static class SystemsOnLoad     
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Execute() => Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("Systems")));
+
+    }
+}
